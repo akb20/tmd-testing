90d63d094d5c870724a55e9c7a6c1ebfc4cb7060
akb20 <aaronkbetts@gmail.com>
2019-08-06 08:14:16 +0800

    added formspree

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,6 +4,14 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+
+    <h3>Get In Touch ✏️</h3>
+    <form action="https://formspree.io/thememedepot@protonmail.com" method="POST">
+      <input type="text" name="name">
+      <input type="email" name="_replyto">
+      <input type="submit" value="Send">
+    </form>
+
   </Layout>
 </template>
 
@@ -14,3 +22,7 @@ export default {
   }
 };
 </script>
+ 
+ <style lang="scss" scoped>
+</style>
+ 
